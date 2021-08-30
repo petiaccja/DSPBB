0ceab9069c3af44dafd95fd771f397569f8ec2a1
Péter Kardos <kardospeter1994@hotmail.com>
2021-08-30 22:08:17 +0200

    test coverage for small filters

--- test/Test_Hilbert.cpp
+++ test/Test_Hilbert.cpp
@@ -29,6 +29,29 @@ TEST_CASE("Form Type IV", "[Hilbert]") {
 	REQUIRE(Min(secondHalf) > 0.0f);
 }
 
+TEST_CASE("Form Type III small", "[Hilbert]") {
+	const auto filter = HilbertFirWinIII<float, TIME_DOMAIN>(19, windows::hamming);
+	REQUIRE(filter.Size() == 19);
+	const auto nonZeroSamples = Decimate(filter, 2);
+	const auto zeroSamples = Decimate(AsView(filter).SubSignal(1), 2);
+	REQUIRE(Max(zeroSamples) == 0.0f);
+	REQUIRE(Min(Abs(nonZeroSamples)) > 0.0f);
+	const auto firstHalf = AsView(nonZeroSamples).SubSignal(0, nonZeroSamples.Size() / 2);
+	const auto secondHalf = AsView(nonZeroSamples).SubSignal(nonZeroSamples.Size() / 2);
+	REQUIRE(Max(firstHalf) < 0.0f);
+	REQUIRE(Min(secondHalf) > 0.0f);
+}
+
+TEST_CASE("Form Type IV small", "[Hilbert]") {
+	const auto filter = HilbertFirWinIV<float, TIME_DOMAIN>(10, windows::hamming);
+	REQUIRE(filter.Size() == 10);
+	REQUIRE(Min(Abs(filter)) > 0.0f);
+	const auto firstHalf = AsView(filter).SubSignal(0, filter.Size() / 2);
+	const auto secondHalf = AsView(filter).SubSignal(filter.Size() / 2);
+	REQUIRE(Max(firstHalf) < 0.0f);
+	REQUIRE(Min(secondHalf) > 0.0f);
+}
+
 constexpr size_t testSignalSize = 4096;
 
 TEST_CASE("Response type III", "[Hilbert]") {
