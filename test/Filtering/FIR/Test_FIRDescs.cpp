610a0b38a8e82deeb06d9a41774538c8248b805f
Péter Kardos <kardospeter1994@hotmail.com>
2021-09-11 19:19:13 +0200

    fixed  maintainability issues

--- test/Filtering/FIR/Test_FIRDescs.cpp
+++ test/Filtering/FIR/Test_FIRDescs.cpp
@@ -8,13 +8,13 @@ using namespace dspbb;
 // Windowed
 //------------------------------------------------------------------------------
 
-const Signal<float, TIME_DOMAIN> window = { 1, 2, 3 };
+const Signal<float, TIME_DOMAIN> winWindow = { 1, 2, 3 };
 constexpr float winCutoff = 0.3f;
 constexpr float winBandLow = 0.4f;
 constexpr float winBandHigh = 0.6f;
 
 TEST_CASE("Low pass windowed view", "[FIR Descs]") {
-	const auto desc = Lowpass(WINDOWED).Cutoff(winCutoff).Window(window);
+	const auto desc = Lowpass(WINDOWED).Cutoff(winCutoff).Window(winWindow);
 	REQUIRE(desc.cutoff == winCutoff);
 	REQUIRE(desc.window.Size() == 3);
 	REQUIRE(desc.window[0] == 1);
@@ -30,7 +30,7 @@ TEST_CASE("Low pass windowed function", "[FIR Descs]") {
 }
 
 TEST_CASE("High pass windowed view", "[FIR Descs]") {
-	const auto desc = Highpass(WINDOWED).Cutoff(winCutoff).Window(window);
+	const auto desc = Highpass(WINDOWED).Cutoff(winCutoff).Window(winWindow);
 	REQUIRE(desc.cutoff == winCutoff);
 	REQUIRE(desc.window.Size() == 3);
 	REQUIRE(desc.window[0] == 1);
@@ -46,7 +46,7 @@ TEST_CASE("High pass windowed function", "[FIR Descs]") {
 }
 
 TEST_CASE("Band pass windowed view", "[FIR Descs]") {
-	const auto desc = Bandpass(WINDOWED).Band(winBandLow, winBandHigh).Window(window);
+	const auto desc = Bandpass(WINDOWED).Band(winBandLow, winBandHigh).Window(winWindow);
 	REQUIRE(desc.low == winBandLow);
 	REQUIRE(desc.high == winBandHigh);
 	REQUIRE(desc.window.Size() == 3);
@@ -64,7 +64,7 @@ TEST_CASE("Band pass windowed function", "[FIR Descs]") {
 }
 
 TEST_CASE("Band stop windowed view", "[FIR Descs]") {
-	const auto desc = Bandstop(WINDOWED).Band(winBandLow, winBandHigh).Window(window);
+	const auto desc = Bandstop(WINDOWED).Band(winBandLow, winBandHigh).Window(winWindow);
 	REQUIRE(desc.low == winBandLow);
 	REQUIRE(desc.high == winBandHigh);
 	REQUIRE(desc.window.Size() == 3);
@@ -82,7 +82,7 @@ TEST_CASE("Band stop windowed function", "[FIR Descs]") {
 }
 
 TEST_CASE("Arbitrary windowed view", "[FIR Descs]") {
-	const auto desc = Arbitrary(WINDOWED).Response([](float f) { return 1.f; }).Window(window);
+	const auto desc = Arbitrary(WINDOWED).Response([](float f) { return 1.f; }).Window(winWindow);
 	REQUIRE(desc.responseFunc(0.3f) == Approx(1.0f));
 	REQUIRE(desc.window.Size() == 3);
 	REQUIRE(desc.window[0] == 1);
