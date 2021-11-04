cbdfa1a1029744b8a6a16fbf45f076af5ebaf23f
Péter Kardos <kardospeter1994@hotmail.com>
2021-11-04 12:20:27 +0100

    implemented tests for root transforms

--- include/dspbb/Math/RootTransforms.hpp
+++ include/dspbb/Math/RootTransforms.hpp
@@ -13,7 +13,9 @@ namespace impl {
 		using T = remove_complex_t<std::decay_t<decltype(*first)>>;
 		size_t realToReals = 0;
 		size_t realToPairs = 0;
-		assert(impl::AreRootsConjugatePairs(first, last)); // Mistake within DSPBB's transform functions, no exception.
+		if (!impl::AreRootsConjugatePairs(first, last)) {
+			throw std::invalid_argument("Multiple roots must be reals or complex conjugate pairs.");
+		}
 		for (auto it = first; it != last; ++it) {
 			const auto& root = *it;
 			realToReals += size_t(imag(root) == T(0));
@@ -42,6 +44,12 @@ template <class T, size_t Order, class TransformFunc>
 FactoredPolynomial<T> TransformRoots(const FactoredPolynomial<T>& poly, TransformFunc func, size_t numRoots = 0, std::array<std::complex<T>, Order> padRoots = {}) {
 	const auto [realToReals, realToPairs] = impl::CountTransformedRoots(poly.RealRoots().begin(), poly.RealRoots().end(), func);
 	const auto [padReals, padPairs] = impl::CountRoots(padRoots.begin(), padRoots.end());
+	if (numRoots >0  && numRoots < poly.NumRoots()) {
+		throw std::invalid_argument("Number of transformed roots must be >= than number of input roots, or 0 to automatically deduce number.");
+	}
+	if (numRoots == 0) {
+		numRoots = poly.NumRoots();
+	}
 	const size_t numPadSets = numRoots - poly.NumRoots();
 
 	const size_t numReals = realToReals + numPadSets * padReals;
@@ -54,6 +62,7 @@ FactoredPolynomial<T> TransformRoots(const FactoredPolynomial<T>& poly, Transfor
 
 	for (auto& root : poly.RealRoots()) {
 		const auto transformedRoots = func(root);
+		assert(std::distance(std::begin(transformedRoots), std::end(transformedRoots)) == Order);
 		for (const auto& troot : transformedRoots) {
 			if (imag(troot) > T(0)) {
 				*(complexRootIt++) = troot;
@@ -65,6 +74,7 @@ FactoredPolynomial<T> TransformRoots(const FactoredPolynomial<T>& poly, Transfor
 	}
 	for (auto& root : poly.ComplexRoots()) {
 		const auto transformedRoots = func(root);
+		assert(std::distance(std::begin(transformedRoots), std::end(transformedRoots)) == Order);
 		complexRootIt = std::copy(transformedRoots.begin(), transformedRoots.end(), complexRootIt);
 	}
 	for (size_t i = 0; i < numPadSets; ++i) {
