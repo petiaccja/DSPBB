9153334637fee5392ac0ce0ea6cf8a208d41dc1f
Péter Kardos <kardospeter1994@hotmail.com>
2021-11-04 12:56:17 +0100

    implemented IIR descs and tests

--- include/dspbb/Filtering/IIR/Descs.hpp
+++ include/dspbb/Filtering/IIR/Descs.hpp
@@ -27,22 +27,58 @@ namespace impl {
 	// Butterworth descs
 	//------------------------------------------------------------------------------
 
-	template <class ParamType>
-	struct LowpassDesc<IirMethodButterworth, ParamType> {
-		ParamType cutoff;
+	template <template <typename, typename...> class Desc, class ParamType>
+	struct SplitDescButterworth {
+		ParamType cutoff = ParamType(0.5);
 
-		[[nodiscard]] auto Cutoff(ParamType cutoffNew) {
-			return LowpassDesc<IirMethodButterworth, ParamType>{ cutoffNew };
+		template <class NewParamType>
+		[[nodiscard]] auto Cutoff(NewParamType cutoffNew) {
+			return Desc<IirMethodButterworth, NewParamType>{ { std::move(cutoffNew) } };
 		}
 	};
+
+	template <template <typename, typename...> class Desc, class ParamType>
+	struct BandDescButterworth {
+		ParamType low = ParamType(0.25);
+		ParamType high = ParamType(0.75);
+
+		template <class NewParamType>
+		[[nodiscard]] auto Band(NewParamType lowNew, NewParamType highNew) {
+			return Desc<IirMethodButterworth, NewParamType>{ { std::move(lowNew), std::move(highNew) } };
+		}
+	};
+
+	template <class T>
+	struct LowpassDesc<IirMethodButterworth, T>
+		: SplitDescButterworth<LowpassDesc, T> {};
 
 	template <>
-	struct LowpassDesc<IirMethodButterworth> {
-		template <class ParamType>
-		[[nodiscard]] auto Cutoff(ParamType cutoffNew) {
-			return LowpassDesc<IirMethodButterworth, ParamType>{ cutoffNew };
-		}
-	};
+	struct LowpassDesc<IirMethodButterworth>
+		: SplitDescButterworth<LowpassDesc, float> {};
+
+	template <class T>
+	struct HighpassDesc<IirMethodButterworth, T>
+		: SplitDescButterworth<HighpassDesc, T> {};
+
+	template <>
+	struct HighpassDesc<IirMethodButterworth>
+		: SplitDescButterworth<HighpassDesc, float> {};
+
+	template <class T>
+	struct BandpassDesc<IirMethodButterworth, T>
+		: BandDescButterworth<BandpassDesc, T> {};
+
+	template <>
+	struct BandpassDesc<IirMethodButterworth>
+		: BandDescButterworth<BandpassDesc, float> {};
+
+	template <class T>
+	struct BandstopDesc<IirMethodButterworth, T>
+		: BandDescButterworth<BandstopDesc, T> {};
+
+	template <>
+	struct BandstopDesc<IirMethodButterworth>
+		: BandDescButterworth<BandstopDesc, float> {};
 
 } // namespace impl
 
@@ -52,6 +88,21 @@ constexpr impl::IirMethodButterworth BUTTERWORTH;
 template <class Method, std::enable_if_t<std::is_base_of_v<impl::IirMethod, Method>, int> = 0>
 auto Lowpass(Method) {
 	return impl::LowpassDesc<Method>{};
+}
+
+template <class Method, std::enable_if_t<std::is_base_of_v<impl::IirMethod, Method>, int> = 0>
+auto Highpass(Method) {
+	return impl::HighpassDesc<Method>{};
+}
+
+template <class Method, std::enable_if_t<std::is_base_of_v<impl::IirMethod, Method>, int> = 0>
+auto Bandpass(Method) {
+	return impl::BandpassDesc<Method>{};
+}
+
+template <class Method, std::enable_if_t<std::is_base_of_v<impl::IirMethod, Method>, int> = 0>
+auto Bandstop(Method) {
+	return impl::BandstopDesc<Method>{};
 }
 
 
---
+++ test/Filtering/IIR/Test_Descs.cpp
@@ -0,0 +1,34 @@
+#include <dspbb/Filtering/IIR/Descs.hpp>
+#include <catch2/catch.hpp>
+
+using namespace dspbb;
+
+//------------------------------------------------------------------------------
+// Butterworth
+//------------------------------------------------------------------------------
+
+constexpr float butterCutoff = 0.3f;
+constexpr float butterBandLow = 0.4f;
+constexpr float butterBandHigh = 0.6f;
+
+TEST_CASE("Low pass butterworth", "[IIR Descs]") {
+	const auto desc = Lowpass(BUTTERWORTH).Cutoff(butterCutoff);
+	REQUIRE(desc.cutoff == butterCutoff);
+}
+
+TEST_CASE("High pass butterworth", "[IIR Descs]") {
+	const auto desc = Highpass(BUTTERWORTH).Cutoff(butterCutoff);
+	REQUIRE(desc.cutoff == butterCutoff);
+}
+
+TEST_CASE("Band pass butterworth", "[IIR Descs]") {
+	const auto desc = Bandpass(BUTTERWORTH).Band(butterBandLow, butterBandHigh);
+	REQUIRE(desc.low == butterBandLow);
+	REQUIRE(desc.high == butterBandHigh);
+}
+
+TEST_CASE("Band stop butterworth", "[IIR Descs]") {
+	const auto desc = Bandstop(BUTTERWORTH).Band(butterBandLow, butterBandHigh);
+	REQUIRE(desc.low == butterBandLow);
+	REQUIRE(desc.high == butterBandHigh);
+}
