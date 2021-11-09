7a35a6e6dd71504797f28c79992e91d8087ea00f
Péter Kardos <kardospeter1994@hotmail.com>
2021-11-09 13:46:20 +0100

    iir filter signal functions

--- include/dspbb/Filtering/IIR/Filter.hpp
+++ include/dspbb/Filtering/IIR/Filter.hpp
@@ -11,7 +11,27 @@ auto Filter(const SignalT& signal, const DiscreteTransferFunction<U>& filter, Di
 	SignalT r;
 	r.Reserve(signal.Size());
 	for (auto& sample : signal) {
-		r.PushBack(state.Feed(sample, filter.numerator.Coefficients(), filter.denominator.Coefficients()));
+		r.PushBack(state.Feed(sample, filter));
+	}
+	return r;
+}
+
+template <class SignalT, class T, class U>
+auto Filter(const SignalT& signal, const DiscreteTransferFunction<U>& filter, DirectFormII<T>& state) {
+	SignalT r;
+	r.Reserve(signal.Size());
+	for (auto& sample : signal) {
+		r.PushBack(state.Feed(sample, filter));
+	}
+	return r;
+}
+
+template <class SignalT, class T, class U>
+auto Filter(const SignalT& signal, const CascadedBiquad<U>& filter, CascadedForm<T>& state) {
+	SignalT r;
+	r.Reserve(signal.Size());
+	for (auto& sample : signal) {
+		r.PushBack(state.Feed(sample, filter));
 	}
 	return r;
 }
