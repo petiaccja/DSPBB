09f4382e5b033b8fdea0cabd86bdecf69fbd67f0
Péter Kardos <kardospeter1994@hotmail.com>
2021-11-28 13:18:20 +0100

    extended tests to fresh structures

--- test/Filtering/IIR/Test_Descs.cpp
+++ test/Filtering/IIR/Test_Descs.cpp
@@ -12,6 +12,13 @@ constexpr float butterCutoff = 0.3f;
 constexpr float butterBandLow = 0.4f;
 constexpr float butterBandHigh = 0.6f;
 
+TEST_CASE("Fresh butterworth", "[IIR Descs]") {
+	REQUIRE(Lowpass(BUTTERWORTH).Cutoff(butterCutoff).cutoff == butterCutoff);
+
+	REQUIRE(Bandpass(BUTTERWORTH).Band(butterBandLow, butterBandHigh).lower == butterBandLow);
+	REQUIRE(Bandpass(BUTTERWORTH).Band(butterBandLow, butterBandHigh).upper == butterBandHigh);
+}
+
 TEST_CASE("Low pass butterworth", "[IIR Descs]") {
 	const auto desc = Lowpass(BUTTERWORTH).Cutoff(butterCutoff);
 	REQUIRE(desc.cutoff == butterCutoff);
@@ -43,6 +50,15 @@ constexpr float cheby1Cutoff = 0.31f;
 constexpr float cheby1Ripple = 0.05f;
 constexpr float cheby1BandLow = 0.41f;
 constexpr float cheby1BandHigh = 0.61f;
+
+TEST_CASE("Fresh chebyshev 1", "[IIR Descs]") {
+	REQUIRE(Lowpass(CHEBYSHEV1).Cutoff(cheby1Cutoff).cutoff == cheby1Cutoff);
+	REQUIRE(Lowpass(CHEBYSHEV1).PassbandRipple(cheby1Ripple).passbandRipple == cheby1Ripple);
+
+	REQUIRE(Bandpass(CHEBYSHEV1).Band(cheby1BandLow, cheby1BandHigh).lower == cheby1BandLow);
+	REQUIRE(Bandpass(CHEBYSHEV1).Band(cheby1BandLow, cheby1BandHigh).upper == cheby1BandHigh);
+	REQUIRE(Bandpass(CHEBYSHEV1).PassbandRipple(cheby1Ripple).passbandRipple == cheby1Ripple);
+}
 
 TEST_CASE("Low pass chebyshev 1", "[IIR Descs]") {
 	const auto desc = Lowpass(CHEBYSHEV1).Cutoff(cheby1Cutoff).PassbandRipple(cheby1Ripple);
@@ -80,6 +96,15 @@ constexpr float cheby2Ripple = 0.04f;
 constexpr float cheby2BandLow = 0.42f;
 constexpr float cheby2BandHigh = 0.62f;
 
+TEST_CASE("Fresh chebyshev 2", "[IIR Descs]") {
+	REQUIRE(Lowpass(CHEBYSHEV2).Cutoff(cheby2Cutoff).cutoff == cheby2Cutoff);
+	REQUIRE(Lowpass(CHEBYSHEV2).StopbandRipple(cheby2Ripple).stopbandRipple == cheby2Ripple);
+
+	REQUIRE(Bandpass(CHEBYSHEV2).Band(cheby2BandLow, cheby2BandHigh).lower == cheby2BandLow);
+	REQUIRE(Bandpass(CHEBYSHEV2).Band(cheby2BandLow, cheby2BandHigh).upper == cheby2BandHigh);
+	REQUIRE(Bandpass(CHEBYSHEV2).StopbandRipple(cheby2Ripple).stopbandRipple == cheby2Ripple);
+}
+
 TEST_CASE("Low pass chebyshev 2", "[IIR Descs]") {
 	const auto desc = Lowpass(CHEBYSHEV2).Cutoff(cheby2Cutoff).StopbandRipple(cheby2Ripple);
 	REQUIRE(desc.cutoff == cheby2Cutoff);
@@ -116,6 +141,17 @@ constexpr float ellipticPassRipple = 0.03f;
 constexpr float ellipticStopRipple = 0.07f;
 constexpr float ellipticBandLow = 0.43f;
 constexpr float ellipticBandHigh = 0.63f;
+
+TEST_CASE("Fresh elliptic", "[IIR Descs]") {
+	REQUIRE(Lowpass(ELLIPTIC).Cutoff(ellipticCutoff).cutoff == ellipticCutoff);
+	REQUIRE(Lowpass(ELLIPTIC).PassbandRipple(ellipticPassRipple).passbandRipple == ellipticPassRipple);
+	REQUIRE(Lowpass(ELLIPTIC).StopbandRipple(ellipticStopRipple).stopbandRipple == ellipticStopRipple);
+
+	REQUIRE(Bandpass(ELLIPTIC).Band(ellipticBandLow, ellipticBandHigh).lower == ellipticBandLow);
+	REQUIRE(Bandpass(ELLIPTIC).Band(ellipticBandLow, ellipticBandHigh).upper == ellipticBandHigh);
+	REQUIRE(Bandpass(ELLIPTIC).PassbandRipple(ellipticPassRipple).passbandRipple == ellipticPassRipple);
+	REQUIRE(Bandpass(ELLIPTIC).StopbandRipple(ellipticStopRipple).stopbandRipple == ellipticStopRipple);
+}
 
 TEST_CASE("Low pass elliptic", "[IIR Descs]") {
 	const auto desc = Lowpass(ELLIPTIC).Cutoff(ellipticCutoff).PassbandRipple(ellipticPassRipple).StopbandRipple(ellipticStopRipple);
