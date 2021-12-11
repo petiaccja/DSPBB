4dd5dd2339a1011b6696b2f4d7aa9d5dc2f8e6f3
Péter Kardos <kardospeter1994@hotmail.com>
2021-12-11 22:03:56 +0100

    added newton-raphson solver

--- include/dspbb/Math/Solvers.hpp
+++ include/dspbb/Math/Solvers.hpp
@@ -3,7 +3,10 @@
 namespace dspbb {
 
 template <class Func, class T>
-T Bisect(Func func, T a, T b) {
+T Bisect(Func func, T a, T b, T tolerance = T(0)) {
+	if (b < a) {
+		std::swap(a, b);
+	}
 	T c = (a + b) / T(2);
 	do {
 		const auto fa = func(a);
@@ -15,9 +18,19 @@ T Bisect(Func func, T a, T b) {
 			a = c;
 		}
 		c = (a + b) / T(2);
-	} while (c != a && c != b);
+	} while (b - c > tolerance && c - a > tolerance);
 
 	return c;
+}
+
+template <class Func, class Derivative, class T>
+T NewtonRaphson(Func func, Derivative der, T x0, T tolerance = T(0)) {
+	T x1 = x0;
+	do {
+		x0 = x1;
+		x1 = x0 - func(x0) / der(x0);
+	} while (std::abs(x1 - x0) > tolerance);
+	return x1;
 }
 
 } // namespace dspbb
--- test/Math/Test_Solvers.cpp
+++ test/Math/Test_Solvers.cpp
@@ -28,3 +28,18 @@ TEST_CASE("Bisect not containing root termination", "[Solvers]") {
 	const auto x0 = Bisect(f, -2.0, -1.0);
 	REQUIRE((x0 == Approx(-2.0) || x0 == Approx(-1.0)));
 }
+
+
+TEST_CASE("Newton exponential", "[Solvers]") {
+	const auto f = [](auto x) { return std::exp(x) - 2; };
+	const auto df = [](auto x) { return std::exp(x); };
+	const auto x0 = NewtonRaphson(f, df, 0.7);
+	REQUIRE(x0 == Approx(std::log(2)));
+}
+
+TEST_CASE("Newton sine", "[Solvers]") {
+	const auto f = [](auto x) { return std::sin(x); };
+	const auto df = [](auto x) { return std::cos(x); };
+	const auto x0 = NewtonRaphson(f, df, 3.0);
+	REQUIRE(x0 == Approx(pi_v<double>));
+}
