0550d604753c55c2ba3acf08418e1227d3995fec
Péter Kardos <kardospeter1994@hotmail.com>
2024-10-07 20:29:10 +0200

    fix sonar & formatting

--- include/dspbb/Math/FFT.hpp
+++ include/dspbb/Math/FFT.hpp
@@ -1,10 +1,9 @@
 #pragma once
 
 #include "../Math/Functions.hpp"
+#include "../PocketFFT/pocketfft_hdronly.h"
 #include "../Primitives/Signal.hpp"
 #include "../Primitives/SignalView.hpp"
-
-#include "../PocketFFT/pocketfft_hdronly.h"
 
 #include <algorithm>
 
